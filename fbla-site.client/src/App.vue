4e142596fbf0cb76339554410cd8ee82dbb0e299
peyton feist <thebindolin@gmail.com>
2025-03-26 23:24:42 -0500

    Added NavBar features, comment's are made for this inside. Still need future styling related updates pending.

--- fbla-site.client/src/App.vue
+++ fbla-site.client/src/App.vue
@@ -1,17 +1,75 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router';
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
-
 <template>
-    <nav class="navbar">
-    <RouterLink class="nav-link" to="/">Go to Home</RouterLink>
-    <RouterLink class="nav-link" to="/AddPosting">Add A Posting</RouterLink>
+  <nav class="navbar">
+    <div class="brand">Summer Spring's high school or something</div>
+    <div class="nav-links">
+      <RouterLink class="nav-link" to="/">Home</RouterLink>
+      <RouterLink class="nav-link" to="/AddPosting">Add A Posting</RouterLink>
+    </div>
   </nav>
+
   <main>
     <RouterView />
   </main>
 </template>
 
 <style scoped>
+/*NavBar Styling*/
+.navbar {
+  position: fixed;
+  top: 0;
+  z-index: 1000; /*The overlay power (lower < higher)*/
+  width: 100%;
+  background: linear-gradient(to right, #1a1a1a, #2a2b2b); /*Changes NavBar background color*/
+  padding: .75rem 1rem; 
+  display: flex;
+  align-items: center; /*Just makes values in middle vertically*/
+  justify-content: space-between;
+  box-shadow: 0 2px 8px rgba(0, 255, 255, 0.247); /*Gives ambiance*/
+}
+
+/*School Text*/
+.brand {
+  color: #ffffff;
+  font-size: 1.5rem;
+  font-weight: 750; /*100-1000 higher = more bold*/
+}
+
+/*Container*/
+.nav-links {
+  display: flex;
+  gap: 2rem;
+}
+
+/* LINKS */
+.nav-link {
+  color: #9be9ff;
+  font-size: 1rem;
+  font-weight: 500;
+  transition: color 0.55s ease;
+}
+
+/*Simply highlights what link/tab their on*/
+/* .router-link-active {
+  color: white;
+  border-bottom: 2px solid #9be9ff;
+} */
+
+
+/*LINK INTERACTION*/
+.nav-link:hover,
+.nav-link:focus {
+  color: #ffffff;
+  outline: none;
+}
+</style>
+
+<!--MNavbar wont cover over the other-->
+<style>
+body {
+  padding-top: 64px;
+}
 </style>
